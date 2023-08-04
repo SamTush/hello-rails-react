fc1b1d031df56e9ecf669c7ce42a7a9cfa88bb0a
Samson Tush <samo4421@icloud.com>
2023-08-04 09:28:21 +0300

    Add linter files

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -4,12 +4,11 @@ const App = () => {
   const [randomGreeting, setRandomGreeting] = useState('');
 
   useEffect(() => {
-    // Fetch random greeting from the API
     fetch('/random_greeting')
       .then(response => response.json())
       .then(data => setRandomGreeting(data.message))
       .catch(error => console.error('Error fetching greeting:', error));
-  }, []); // Empty dependency array to ensure this effect runs only once on component mount
+  }, []);
 
   return (
     <div>
